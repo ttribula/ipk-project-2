99fc4440166fb09a46248da28684e6aa85691a88
xtribu02 <tribula.tomas@gmail.com>
2020-05-02 09:39:47 +0200

    final base task

--- ipk-sniffer.c
+++ ipk-sniffer.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include "string.h"
 #include "netdb.h"
-#include "sys/types.h"
-#include "sys/socket.h"
-#include "arpa/inet.h"
 #include "netinet/ip.h"
 #include "netinet/tcp.h"
 #include "netinet/udp.h"
-#include "netinet/ip_icmp.h"
 #include "netinet/if_ether.h"
-#include "netinet/ip6.h"
-#include "unistd.h"
+#include "sys/socket.h"
+#include "arpa/inet.h"
 #include "pcap.h"
 #include <stdlib.h>
 #include <time.h>
 #include "getopt.h"
 
-
+/**
+ * Struktura Params pro ulozeni a pouziti parametru programu
+ *
+ */
 typedef struct parameters {
     int interface;
     char *interfaceId;
@@ -25,35 +24,32 @@ typedef struct parameters {
     int udp;
     int num;
 } Params;
-Params params;
-int totalPackets = 0;
-struct sockaddr_in source, dest;
 
+// globalni promenne
+Params params;          //parametry programu
+int totalPackets = 0;   //celkovy pocet odchycenych paketu
 
+// prototypy funkci
 int parseArgs(int argc, char *argv[]);
-
-void processPacket(const struct pcap_pkthdr *header, const u_char *buffer);
-
 void printHeader(const u_char *buffer, unsigned short sourcePort, unsigned short destPort);
-
 void printPacket(const u_char *buffer, int len);
-
 void tcpPacket(const u_char *buffer, int len);
-
 void udpPacket(const u_char *buffer, int len);
+
 
 int main(int argc, char *argv[]) {
     pcap_t *handle;
     struct pcap_pkthdr *header;
+    struct iphdr *ipHeader;
     const u_char *packet;
     int res;
     char errorBuffer[100];
 
     parseArgs(argc, argv);
 
-
+    // pokud nebylo zadane rozhrani, vypise vsechny rozhrani a ukonci program
     if (params.interface == 0) {
-        pcap_if_t *devList, *device;;
+        pcap_if_t *devList, *device;
         pcap_findalldevs(&devList, errorBuffer);
         printf("Seznam aktivnich rozhrani:\n");
         for (device = devList; device != NULL; device = device->next) {
@@ -63,55 +59,61 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
+    // otevreni rozhrani pro odchyceni paketu
     handle = pcap_open_live(params.interfaceId, 65536, 1, 1000, errorBuffer);
     if (!handle) {
+        // nastala chyba pri otevirani rozhrani
         printf("%s\n", errorBuffer);
         return 1;
     }
 
+    // dokud nebyl zpracovan pozadovany pocet paketu a je odchyceny dalsi paket
     while (totalPackets < params.num && (res = pcap_next_ex(handle, &header, &packet))) {
+        // pokud program nestihl precist cely paket, pokracuje v cyklu
         if (res == 0) {
             continue;
         }
-
-        processPacket(header, packet);
+        // ulozeni ip hlavicky z paketu do struktury
+        ipHeader = (struct iphdr *) (packet + sizeof(struct ethhdr));
+        if (ipHeader->protocol == 6) {          // TCP
+            // kontrola zda byl pouzit parametr -t nebo --tcp nebo zadny z nich
+            if ((params.tcp == 0 && params.udp == 0) || params.tcp == 1) {
+                tcpPacket(packet, header->len);
+            }
+        } else if (ipHeader->protocol == 17) {  // UDP
+            // kontrola zda byl pouzit parametr -u nebo --udp nebo zadny z nich
+            if ((params.tcp == 0 && params.udp == 0) || params.udp == 1) {
+                udpPacket(packet, header->len);
+            }
+        }
     }
-
+    // uvolneni pameti nazvu zadaneho rozhrani
     free(params.interfaceId);
     return 0;
 }
 
-void processPacket(const struct pcap_pkthdr *header, const u_char *buffer) {
-    struct iphdr *ipHeader = (struct iphdr *) (buffer + sizeof(struct ethhdr));
-    switch (ipHeader->protocol) {
-        case 6:
-            if ((params.tcp == 0 && params.udp == 0) || params.tcp == 1) {
-                tcpPacket(buffer, header->len);
-                ++totalPackets;
-            }
-            break;
-        case 17:
-            if ((params.tcp == 0 && params.udp == 0) || params.udp == 1) {
-                udpPacket(buffer, header->len);
-                ++totalPackets;
-            }
-            break;
-        default:
-            break;
-    }
-}
-
+/**
+ * Funkce vypise zakladni informace o paketu, jako jsou cas odchyceni, zdrojova a cilova ip adresa a port atd.
+ *
+ * @param buffer Data paketu
+ * @param sourcePort Zdrojovy port, pro nastaveni filtru na port
+ * @param destPort Cilovy port, pro nastaveni filtru na port
+ */
 void printHeader(const u_char *buffer, unsigned short sourcePort, unsigned short destPort) {
     struct addrinfo *res, *result;
     char srcHostname[1025] = "";
     char destHostname[1025] = "";
     struct iphdr *ipHeader = (struct iphdr *) (buffer + sizeof(struct ethhdr));
     int err;
+    struct sockaddr_in source, dest;
+
+    // ulozeni ip adres do struktur pro nasledne uziti adres
     memset(&source, 0, sizeof(source));
     source.sin_addr.s_addr = ipHeader->saddr;
     memset(&dest, 0, sizeof(dest));
     dest.sin_addr.s_addr = ipHeader->daddr;
 
+    // pokus o ziskani domenoveho jmena zdrojove ip adresy
     err = getaddrinfo(inet_ntoa(source.sin_addr), NULL, NULL, &result);
     if (err != 0) {
         printf("err getaddrinfo\n");
@@ -126,9 +128,11 @@ void printHeader(const u_char *buffer, unsigned short sourcePort, unsigned short
             break;
         }
     }
+    // pokud nebylo nalezeno domenove jmeno, pouzije se ip adresa
     if (!strcmp(srcHostname, "")) {
         strcpy(srcHostname, inet_ntoa(source.sin_addr));
     }
+    // pokus o ziskani domenoveho jmena cilove ip adresy
     err = getaddrinfo(inet_ntoa(dest.sin_addr), NULL, NULL, &result);
     if (err != 0) {
         printf("err getaddrinfo\n");
@@ -143,12 +147,13 @@ void printHeader(const u_char *buffer, unsigned short sourcePort, unsigned short
             break;
         }
     }
+    // pokud nebylo nalezeno domenove jmeno, pouzije se ip adresa
     if (!strcmp(destHostname, "")) {
         strcpy(destHostname, inet_ntoa(dest.sin_addr));
     }
     freeaddrinfo(result);
 
-
+    // ziskani aktualniho casu
     char buf[80];
     struct timeval time;
     gettimeofday(&time, NULL);
@@ -157,23 +162,34 @@ void printHeader(const u_char *buffer, unsigned short sourcePort, unsigned short
     ts = *localtime(&raw);
     strftime(buf, sizeof(buf), "%H:%M:%S", &ts);
     int usec = (int) time.tv_usec;
-    gethostbyaddr(inet_ntoa(source.sin_addr), 0, 0);
 
+    // vypis zakladnich informaci: cas ip/domenove_jmeno port > ip/domenove_jmeno port
     printf("%s.%d %s : %d > %s : %d\n\n", buf, usec, srcHostname, sourcePort,
            destHostname, destPort);
 }
 
+/**
+ * Funkce vypise data celeho paketu v hexadecimalnim a ascii formatu
+ *
+ * @param buffer Data paketu
+ * @param len Delka paketu
+ */
 void printPacket(const u_char *buffer, int len) {
+    // ocislovani radku
     int hexnum = 0;
+    // vypis celeho paketu
     for (int i = 0; i < len; ++i) {
+        // hexadecimalni format - po 16 bitech
         if (i != 0 && i % 16 == 0) {
             printf("%#06x: ", hexnum);
+            //vypis hexa
             for (int j = i - 16; j < i; ++j) {
                 if (j % 8 == 0) {
                     printf(" ");
                 }
                 printf("%02x ", (unsigned int) buffer[j]);
             }
+            // vypis ascii
             for (int j = i - 16; j < i; ++j) {
                 if (j % 8 == 0) {
                     printf(" ");
@@ -184,18 +200,6 @@ void printPacket(const u_char *buffer, int len) {
                     printf(".");
                 }
             }
-            if (i == len - 1) {
-                for (int j = 0; j < 15 - (i % 16); ++j) {
-
-                }
-                for (int j = i - i % 16; j <= i; ++j) {
-                    if (buffer[j] >= 32 && buffer[j] <= 128) {
-                        printf("%c", (unsigned char) buffer[j]);
-                    } else {
-                        printf(".");
-                    }
-                }
-            }
             hexnum += 16;
             printf("\n");
         }
@@ -203,6 +207,12 @@ void printPacket(const u_char *buffer, int len) {
     printf("\n");
 }
 
+/**
+ * Funkce ulozi data o tcp paketu do struktury, pro ziskani potrebnych dat a vola vypis celeho paketu
+ *
+ * @param buffer Data paketu
+ * @param len Delka paketu
+ */
 void tcpPacket(const u_char *buffer, int len) {
     struct iphdr *ipHeader = (struct iphdr *) (buffer + sizeof(struct ethhdr));
 
@@ -212,9 +222,17 @@ void tcpPacket(const u_char *buffer, int len) {
         printHeader(buffer, ntohs(tcpHeader->source), ntohs(tcpHeader->dest));
 
         printPacket(buffer, len);
+
+        ++totalPackets;
     }
 }
 
+/**
+ * Funkce ulozi data o udp paketu do struktury, pro ziskani potrebnych dat a vola vypis celeho paketu
+ *
+ * @param buffer Data paketu
+ * @param len Delka paketu
+ */
 void udpPacket(const u_char *buffer, int len) {
     struct iphdr *ipHeader = (struct iphdr *) (buffer + sizeof(struct ethhdr));
 
@@ -224,9 +242,18 @@ void udpPacket(const u_char *buffer, int len) {
         printHeader(buffer, ntohs(udpHeader->source), ntohs(udpHeader->dest));
 
         printPacket(buffer, len);
+
+        ++totalPackets;
     }
 }
 
+/**
+ * Funkce na parsovani argumentu programu do struktury Params
+ *
+ * @param argc Pocet argumentu programu
+ * @param argv Argumenty programu
+ * @return Chybovy kod
+ */
 int parseArgs(int argc, char *argv[]) {
     params.interface = 0;
     params.interfaceId = (char *) malloc(sizeof(optarg));
